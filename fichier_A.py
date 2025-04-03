1246dd8746ce189af2f2a680a9a26cbf3b2aebcf
Vincent Archambault-Bouffard <varchambaultbouffard@cmaisonneuve.qc.ca>
2025-04-03 13:05:08 -0400

    Validation initiale

---
+++ fichier_A.py
@@ -0,0 +1,14 @@
+def division(a, b):
+    result = a / b
+    return result
+
+
+def boucle():
+    i = 9
+    while i != 10:
+        print("Il reste ...", i, "itérations")
+        i -= 1
+
+
+def plus_deux(a):
+    return b + 2
